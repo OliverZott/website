545bc0ab42944e57d88ff005c776b18e9604d749
Oliver Zott <oliver.zott@vertigis.com>
2023-05-19 14:40:57 +0200

    Blog - WIP

--- components/Blog.tsx
+++ components/Blog.tsx
@@ -13,6 +13,23 @@ interface BlogProps {
 
 const Blog: React.FC<BlogProps> = ({ posts }) => {
 
+
+    function renderBlog(post: BlogProps) {
+        return (
+            <div>
+                <h1>Blog</h1>
+                <p>Test</p>
+
+                <li key={post.slug}>
+                    <h2>{post.title}</h2>
+                    <p>Date: {post.date}</p>
+                    <p>Author: {post.author}</p>
+                </li>
+            </div>
+        )
+    }
+
+
     return (
         <main>
             <div>
